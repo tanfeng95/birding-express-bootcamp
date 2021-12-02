d7727fc1a9cf6f6be19791a2296e1e5df7d2e39d
tanfeng95 <tanfeng95@hotmail.com>
2021-12-02 21:48:32 +0800

    2nd commit

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const getAllNotes = (req, res) => {
 
     // console.log(result.rows);
     const notesArray = { notes: result.rows };
-    res.render('listOfNotes', notesArray);
+    res.render('index', notesArray);
   };
   // Query using pg.Pool instead of pg.Client
   pool.query(getAllNoteQueryString, whenDoneWithQuery);
@@ -49,7 +49,20 @@ const getNoteById = (req, res) => {
     }
     const newResult = result.rows.pop();
     console.log(newResult);
-    res.render('singleNote', newResult);
+    const getCommentByID = `select * from comments where note_id =${newResult.id}`;
+    pool.query(getCommentByID, (err, result2) => {
+      if (err) {
+        console.log('Error executing query', err.stack);
+        res.status(503).send(result2.rows);
+      } else {
+        const data = {
+          note: newResult,
+          comments: result2.rows,
+        };
+        console.log(data);
+        res.render('singleNote', data);
+      }
+    });
   };
 
   getSingleNoteQueryString = `SELECT * from notes where id = '${req.params.id}'`;
@@ -58,7 +71,31 @@ const getNoteById = (req, res) => {
 };
 
 const getPostNote = (req, res) => {
-  res.render('noteForm');
+  const getsSpecies = 'select * from species';
+
+  const whenDoneWithQuery = (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      res.status(503).send(result.rows);
+      return;
+    }
+    const getBehaviourQuery = 'select * from behaviour';
+
+    pool.query(getBehaviourQuery, (err, result2) => {
+      if (err) {
+        console.log('Error executing query', err.stack);
+        res.status(503).send(result2.rows);
+      } else {
+        const data = {
+          behaviour: result2.rows,
+          species: result.rows,
+        };
+        console.log(data);
+        res.render('noteForm', data);
+      }
+    });
+  };
+  pool.query(getsSpecies, whenDoneWithQuery);
 };
 
 const PostPostNote = (req, res) => {
@@ -70,17 +107,31 @@ const PostPostNote = (req, res) => {
       res.status(503).send(result.rows);
       return;
     }
-    res.redirect('/');
+    const postId = result.rows[0].id;
+    let queryDoneCounter = 0;
+    req.body.behaviour_id.forEach((behaviourId, index) => {
+      const param2 = [postId, behaviourId];
+      const noteBehaviourQuery = 'INSERT INTO note_behaviour (note_id,behaviour_id) values ($1,$2)';
+      pool.query(noteBehaviourQuery, param2, (err, result2) => {
+        console.log('result2');
+        queryDoneCounter += 1;
+        if (queryDoneCounter === req.body.behaviour_id.length) {
+          // TODO: check if any of the queries had errors.
+          // all the queries are done, send a response.
+          res.redirect('/');
+        }
+      });
+    });
   };
   const inputData = [req.body.habitat, req.body.date, req.body.appearance,
-    req.body.behaviour, req.body.vocalisations, req.body.flockSize];
+    req.body.behaviour, req.body.vocalisations, req.body.flockSize, req.body.Species_name];
 
   if (req.cookies.loggedIn === undefined) {
     res.render('userIsNotLogin');
     return;
   }
 
-  const queryString = 'INSERT INTO notes (habitat,date,appearance,behaviour,vocalisations,flockSize) VALUES ($1,$2,$3,$4,$5,$6)';
+  const queryString = 'INSERT INTO notes (habitat,date,appearance,behaviour,vocalisations,flockSize,species) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING *';
   pool.query(queryString, inputData, whenDoneWithQuery);
 };
 
@@ -191,6 +242,82 @@ const deleteCookie = (req, res) => {
   res.render('logout');
 };
 
+const getSpecies = (req, res) => {
+  res.render('speciesForm');
+};
+
+const postSpecies = (req, res) => {
+  console.log('request came in');
+  console.log(req.body);
+  const whenDoneWithQuery = (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      res.status(503).send(result.rows);
+      return;
+    }
+    res.redirect('/');
+  };
+  const inputData = [req.body.name, req.body.scientific_name];
+
+  if (req.cookies.loggedIn === undefined) {
+    res.render('userIsNotLogin');
+    return;
+  }
+
+  const queryString = 'INSERT INTO species (name,scientific_name) VALUES ($1,$2)';
+  pool.query(queryString, inputData, whenDoneWithQuery);
+};
+
+const allSpecies = (req, res) => {
+  console.log('request came in');
+  const whenDoneWithQuery = (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      res.status(503).send(result.rows);
+      return;
+    }
+    // console.log(result.rows);
+    const speciesArray = { notes: result.rows };
+    res.render('species', speciesArray);
+  };
+  const queryString = 'select * from species;';
+  pool.query(queryString, whenDoneWithQuery);
+};
+
+const getPutSpeciesForm = (req, res) => {
+  const whenDoneWithQuery = (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      res.status(503).send(result.rows);
+      return;
+    }
+    console.log(result.rows);
+    const newResult = result.rows.pop();
+
+    res.render('editForm', newResult);
+  };
+
+  getSingleNoteQueryString = `SELECT * from species where id = '${req.params.id}'`;
+  console.log(req.params.id);
+  pool.query(getSingleNoteQueryString, whenDoneWithQuery);
+};
+
+const postComments = (req, res) => {
+  console.log('request came in');
+  console.log(req.body);
+  const whenDoneWithQuery = (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      res.status(503).send(result.rows);
+    }
+    res.redirect('/');
+  };
+  const inputData = [req.body.comment, req.body.id];
+
+  const queryString = 'INSERT INTO comments (commments,note_id) VALUES ($1,$2)';
+  pool.query(queryString, inputData, whenDoneWithQuery);
+};
+
 app.get('/', getAllNotes);
 app.get('/note/:id', getNoteById);
 app.get('/note', getPostNote);
@@ -203,4 +330,9 @@ app.post('/login', postLogin);
 app.get('/signUp', getSignUpPage);
 app.post('/signUp', postSignUp);
 app.delete('/logout', deleteCookie);
+app.get('/species', getSpecies);
+app.post('/species', postSpecies);
+app.get('/species/all', allSpecies);
+app.get('/species/:index/edit', getPutSpeciesForm);
+app.post('/note/:id/comment', postComments);
 app.listen(3004);
